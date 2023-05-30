1d7e2d4d1fb17510ffc05c37b255c735408656a9
Devolation1 <daneyoungquist@gmail.com>
2023-05-30 14:41:24 -0700

    js script

--- wellandtable/js/form.js
+++ wellandtable/js/form.js
@@ -1,25 +1,28 @@
 let fname = "";
 
-// function to get info from intro form
-// check input and produce a sentence
+
 function greet(){
     let greetParagraph = document.getElementById("greet");
 
-    // get the values from the form
+    
     fname = document.getElementById("fname").value ;
     let lname = document.getElementById("lname").value;
     let age = document.getElementById("age").value;
 
-    // check the input
+    
     if (fname==="" || lname==="" ){
         alert("First and last name cannot be an empty string!");
         return;
     }
-    // age check
-    if (age==="" || age < 18){
+    
+    if (age==="" || age < 21){
         alert("Invalid age! You must be 21 or older!")
         return;
     }
-    // update output paragraph
+    else{
+        document.getElementById('demo').style.display='block';
+    }
+    
+    
     greetParagraph.innerHTML="You May Now Look at The Acoholic Beverages " + fname;
 }
